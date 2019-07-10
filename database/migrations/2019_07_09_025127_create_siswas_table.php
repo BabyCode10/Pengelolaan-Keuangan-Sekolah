b22a4c102d12f7c3a6a1d9c01669ddd95e164555
irfan hadian <irfan.hadian10@gmail.com>
2019-07-10 11:33:28 +0700

    Initial commit

---
+++ database/migrations/2019_07_09_025127_create_siswas_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nis', 50)->unique();
+            $table->string('nama', 50);
+            $table->text('alamat');
+            $table->enum('jenis_kelamin', [
+                'laki-laki', 
+                'perempuan',
+            ]);
+            $table->enum('agama', [
+                'islam',
+                'kristen',
+                'hindu',
+                'budha',
+            ]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswas');
+    }
+}
---
+++ database/migrations/2019_07_09_025945_create_transaksis_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransaksisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nis', 50);
+            $table->string('nama', 50);
+            $table->string('kelas', 50);
+            $table->string('jenis_bayaran', 25);
+            $table->double('jumlah_bayaran', 1, 2);
+            $table->string('petugas', 50);
+            $table->timestamps();
+
+            $table->foreign('nis')
+                ->references('nis')
+                ->on('siswas')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksis');
+    }
+}
